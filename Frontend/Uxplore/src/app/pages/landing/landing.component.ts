4b61e3e85ee7624cc5d7969f2b9d4ec52b25b17d
Louis batty <93161778+Uncle-batty@users.noreply.github.com>
2024-05-18 13:40:25 +0200

    Login Pop up

--- Frontend/Uxplore/src/app/pages/landing/landing.component.ts
+++ Frontend/Uxplore/src/app/pages/landing/landing.component.ts
@@ -1,17 +1,18 @@
 import { IonicModule } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss'],
-  imports : [IonicModule]
+  imports : [IonicModule, CommonModule]
 })
 export class LandingComponent  implements OnInit {
   presentingElement = document.getElementById('0');
+  isModalOpen = false;
   constructor(private router: Router) { }
 
     navpage(path : string) {
@@ -20,6 +21,14 @@ export class LandingComponent  implements OnInit {
 }
   ngOnInit() {
     this.presentingElement = document.querySelector('.ion-page');
+  }
+
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
   }
 
 }

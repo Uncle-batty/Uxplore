82dde3021eb0e31c371693be38f19a1658fd2c36
Tadi-C <tadichingwe0@gmail.com>
2024-05-20 12:08:49 +0200

    Cards

---
+++ Frontend/Uxplore/src/Models/event-card.ts
@@ -0,0 +1,10 @@
+export interface Event {
+  Id: string;
+  Name: string;
+  Location: string;
+  PriceRange: string;
+  Times: string;
+  Rating: string;
+  SafetyRating: string;
+  ImageData: string;
+}
--- Frontend/Uxplore/src/app/pages/individual-category/individual-category.component.ts
+++ Frontend/Uxplore/src/app/pages/individual-category/individual-category.component.ts
@@ -1,24 +1,44 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { EventCardComponent } from 'src/app/event-card/event-card.component';
+import { Event } from 'src/Models/event-card';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-individual-category',
   standalone: true,
   templateUrl: './individual-category.component.html',
   styleUrls: ['./individual-category.component.scss'],
-  imports: [EventCardComponent]
+  imports: [EventCardComponent,CommonModule]
 })
 export class IndividualCategoryComponent  implements OnInit {
-
+  events: Event[] = [];
   constructor(private router: Router) {
 
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.createTestEvents();
+  }
 
   navpage(path : string) {
 
   this.router.navigate([path]);
 }
+
+createTestEvents() {
+    for (let index = 0; index < 6; index++) {
+      let event1: Event = {
+        Id: index.toString(),
+        Name: "RocoMamas",
+        Location: "Campus Square",
+        PriceRange: "70 - 250",
+        Times: "9am - 7pm",
+        Rating: "4",
+        SafetyRating: "Safe",
+        ImageData: ''
+      };
+      this.events.push(event1);
+    }
+  }
 
 }

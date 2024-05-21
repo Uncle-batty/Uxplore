d6ced9f0fbfd6f1bc073a7f7bd438afaf853e16f
Louis batty <93161778+Uncle-batty@users.noreply.github.com>
2024-05-21 21:15:17 +0200

    Added routes to Joses page

--- Frontend/Uxplore/src/app/app.routes.ts
+++ Frontend/Uxplore/src/app/app.routes.ts
@@ -8,6 +8,8 @@ import { CategoryComponent } from './pages/category/category.component';
 import { IndividualCategoryComponent } from './pages/individual-category/individual-category.component';
 import { LandingComponent } from './pages/landing/landing.component';
 import { AboutComponent } from './pages/about/about.component';
+import { OneActivityComponent } from './pages/one-activity/one-activity.component';
+import { ExploreComponent } from './pages/explore/explore.component';
 
 
 const userRoutes: Routes = [
@@ -15,6 +17,8 @@ const userRoutes: Routes = [
   {path: 'calender', component: CalenderComponent},
   {path : 'category', component: CategoryComponent},
   {path : 'individualCategory', component: IndividualCategoryComponent},
+  {path : 'activity', component: OneActivityComponent},
+  {path : 'explore', component: ExploreComponent}
 ];
 
 const mainRoutes: Routes = [
--- Frontend/Uxplore/src/app/pages/explore/explore.component.ts
+++ Frontend/Uxplore/src/app/pages/explore/explore.component.ts
@@ -3,7 +3,7 @@ import { addIcons } from 'ionicons';
 import { arrowForwardCircleOutline } from 'ionicons/icons';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-explore',
   templateUrl: './explore.component.html',
@@ -12,7 +12,12 @@ import { CommonModule } from '@angular/common';
   imports: [IonicModule, CommonModule]
 })
 export class ExploreComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
+
+  navpage(path : string) {
+
+  this.router.navigate([path]);
+}
 
   ngOnInit() {
     addIcons({

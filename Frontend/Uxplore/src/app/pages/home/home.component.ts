1d01470a306a006225fb2005abfcb7aabb9e49d4
Louis batty <93161778+Uncle-batty@users.noreply.github.com>
2024-05-22 16:35:53 +0200

    Removed other frontend

--- Frontend/Uxplore/src/app/pages/home/home.component.ts
+++ Frontend/Uxplore/src/app/pages/home/home.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
-
+import { IonApp, IonRouterOutlet, IonGrid, IonContent, IonCol, IonCard, IonRow, IonCardContent, IonCardHeader, IonCardTitle, IonCardSubtitle } from '@ionic/angular/standalone';
+import { EventCardComponent } from 'src/app/event-card/event-card.component';
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  imports : [IonApp, IonRouterOutlet]
+  imports : [IonApp, IonRouterOutlet,IonGrid, IonCol, IonRow, IonCard, CommonModule,IonContent,IonCardContent,IonCardHeader,IonCardTitle,IonCardSubtitle,EventCardComponent ]
 })
 export class HomeComponent  implements OnInit {
 

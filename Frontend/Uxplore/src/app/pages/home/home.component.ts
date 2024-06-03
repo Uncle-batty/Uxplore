d8e12fb49bb046518d7c39096f8db531a158eb0f
JoseHyman <112486309+JoseHyman@users.noreply.github.com>
2024-06-03 15:00:22 +0200

    Home Page

--- Frontend/Uxplore/src/app/pages/home/home.component.ts
+++ Frontend/Uxplore/src/app/pages/home/home.component.ts
@@ -1,18 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { arrowForwardCircleOutline } from 'ionicons/icons';
 import { IonApp, IonRouterOutlet, IonGrid, IonContent, IonCol, IonCard, IonRow, IonCardContent, IonCardHeader, IonCardTitle, IonCardSubtitle } from '@ionic/angular/standalone';
 import { EventCardComponent } from 'src/app/event-card/event-card.component';
+import { addIcons } from 'ionicons';
+import { IonicModule } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  imports : [IonApp, IonRouterOutlet,IonGrid, IonCol, IonRow, IonCard, CommonModule,IonContent,IonCardContent,IonCardHeader,IonCardTitle,IonCardSubtitle,EventCardComponent ]
+  imports : [IonicModule, CommonModule,EventCardComponent ]
 })
 export class HomeComponent  implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {}
-
+  ngOnInit() {
+    addIcons({
+      'arrow-forward-circle-outline': arrowForwardCircleOutline
+    });
+  }
 }

1b4e41e473eb937369c23098e5828cc639d605d9
Tadi-C <tadichingwe0@gmail.com>
2024-06-25 09:56:44 +0200

    Removed frontEnd

--- Frontend/Uxplore/android/app/src/main/java/com/uxplore/UXPLORE/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.uxplore.UXPLORE;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- Frontend/Uxplore/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.uxplore.UXPLORE',
-  appName: 'Uxplore',
-  webDir: 'www'
-};
-
-export default config;
